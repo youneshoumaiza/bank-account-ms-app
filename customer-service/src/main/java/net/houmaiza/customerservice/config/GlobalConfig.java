10f21100a5b9d1a36bdbfdfed29ef6f6618b4c34
Younes Houmaiza <youneshoumaiza10@gmail.com>
2024-03-05 16:35:38 +0100

    commit

--- customer-service/src/main/java/net/houmaiza/customerservice/config/GlobalConfig.java
+++ customer-service/src/main/java/net/houmaiza/customerservice/config/GlobalConfig.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConfigurationProperties(prefix = "global.params")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
--- customer-service/src/main/java/net/houmaiza/customerservice/web/CustomerRestcontroller.java
+++ customer-service/src/main/java/net/houmaiza/customerservice/web/CustomerRestcontroller.java
@@ -4,7 +4,6 @@ import net.houmaiza.customerservice.entities.Customer;
 import net.houmaiza.customerservice.repository.CustomerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
